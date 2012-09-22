cfcf5168a2e3b83e2dca4d826c2b39c907e6f393
Nikola Vidoni <nikola.vidoni@net.amis.hr>
2012-09-22 11:39:30 +0200

    Initial Commit

---
+++ default.php
@@ -0,0 +1,298 @@
+<?php
+
+/**
+ * ProcessWire 2.x Admin Markup Template / Copyright 2010 by Ryan Cramer
+ *
+ * ProcessWire Ergo Admin Markup Template 1.0 / Copyright 2012 by Nikola Vidoni  
+ *
+ */
+
+Debug::timer('ergo');
+
+$searchForm = $user->hasPermission('page-edit') ? $modules->get('ProcessPageSearch')->renderSearchForm() : '';
+$bodyClass = $input->get->modal ? 'modal' : '';
+if(!isset($content)) $content = '';
+
+$config->styles->prepend($config->urls->adminTemplates . "styles/main.css"); 
+$config->styles->append($config->urls->adminTemplates . "styles/ui.css");
+$config->scripts->append($config->urls->adminTemplates . "scripts/jquery.cookie.js");
+$config->scripts->append($config->urls->adminTemplates . "scripts/jquery.tiptip.js");
+$config->scripts->append($config->urls->adminTemplates . "scripts/main.js");
+
+$browserTitle = wire('processBrowserTitle'); 
+if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FILE__) . ' &bull; ProcessWire';
+
+/*
+ * Dynamic phrases that we want to be automatically translated
+ *
+ * These are in a comment so that they register with the parser, in place of the dynamic __() function calls with page titles. 
+ * 
+ * __("Pages"); 
+ * __("Setup"); 
+ * __("Modules"); 
+ * __("Access"); 
+ * __("Admin"); 
+ * 
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo __('en', __FILE__); // HTML tag lang attribute
+	/* this intentionally on a separate line */ ?>">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="noindex, nofollow" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title><?php echo $browserTitle; ?></title>
+
+	<script type="text/javascript">
+		<?php
+
+		$jsConfig = $config->js();
+		$jsConfig['debug'] = $config->debug;
+		$jsConfig['urls'] = array(
+			'root' => $config->urls->root, 
+			'admin' => $config->urls->admin, 
+			'modules' => $config->urls->modules, 
+			'core' => $config->urls->core, 
+			'files' => $config->urls->files, 
+			'templates' => $config->urls->templates,
+			'adminTemplates' => $config->urls->adminTemplates,
+			); 
+		?>
+
+		var config = <?php echo json_encode($jsConfig); ?>;
+	</script>
+
+	<?php foreach($config->styles->unique() as $file) echo "\n\t<link type='text/css' href='$file' rel='stylesheet' />"; ?>
+
+	<!--[if IE]>
+	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->adminTemplates; ?>styles/ie.css" />
+	<![endif]-->	
+
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->adminTemplates; ?>styles/ie7.css" />
+	<![endif]-->
+
+	<?php foreach($config->scripts->unique() as $file) echo "\n\t<script type='text/javascript' src='$file'></script>"; ?>
+
+</head>
+
+<?php if($user->isGuest()) { ?>
+
+<body class="login">
+
+    <div id="loginBox">
+    
+    	<div class="header">
+        	<div class="logo">ProcessWire</div>
+        </div>
+    
+        <?php echo $content?>
+
+    </div>
+    
+    <?php if(count($notices)) include($config->paths->adminTemplates . "notices.inc"); ?>
+
+</body>
+
+<?php } else { ?>
+
+<body<?php if($bodyClass) echo " class='$bodyClass'"; ?>>
+
+<!-- Wrapper
+-------------------------------------------------------------------->
+
+<div id="wrapper">
+
+    <!-- Toggle Sidebar On/Off
+    -------------------------------------------------------------------->
+    
+    <a id="toggle">Collapse Sidebar</a>
+    
+    <!-- Sidebar
+    -------------------------------------------------------------------->
+
+    <div id="sidebar">
+    
+        <h2>Latest updates</h2>
+    
+        <?php $updates = $pages->find('limit=3, sort=-modified');
+        
+        echo "<ul>";
+        
+		foreach($updates as $update) {
+			if ($update->editable()) {
+				echo "<li><a href='".$config->urls->admin."page/edit/?id={$update->id}'><span class='date'>". date('d.m.', $update->modified) ."</span> " . $update->title . "</a></li>\n";
+			}	
+		}
+		
+		echo "</ul>";
+		
+		?>  
+        
+        <h2>Newest added</h2>
+        
+        <?php $newest = $pages->find('limit=3, sort=-created');
+        
+        echo "<ul>";
+        
+		foreach($newest as $new) {
+			if ($new->editable()) {
+				echo "<li><a href='".$config->urls->admin."page/edit/?id={$new->id}'><span class='date'>". date('d.m.', $new->created) ."</span> " . $new->title . "</a></li>\n";
+			}
+		}
+		
+		echo "</ul>";
+		
+		?>
+
+	</div><!-- Sidebar END-->
+
+	<!-- Content
+    -------------------------------------------------------------------->
+    
+    <div id="content" class="content">
+	
+        <!-- Container
+        -------------------------------------------------------------------->
+            
+        <div class="container">
+    
+            <!-- Left
+            -------------------------------------------------------------------->
+                
+            <div class="left">
+    
+                <!-- Logo
+                -------------------------------------------------------------------->
+                
+                <p id="logo">ProcessWire</p>
+                
+                <!-- Menu
+                -------------------------------------------------------------------->
+                
+                <ul id="nav" class="accordion">
+                    <?php include($config->paths->templatesAdmin . "topnav.inc"); ?>
+                </ul>
+    
+            </div><!-- Left END-->
+            
+            <!-- Right
+            -------------------------------------------------------------------->
+                
+            <div class="right">
+            
+                <!-- Header
+                -------------------------------------------------------------------->
+                
+                <div id="header" class="header">
+                    
+                    <!-- Logged User
+                    -------------------------------------------------------------------->
+                    
+                    <div id="user">You are logged in as <span><?php echo $user->name?></span></div>
+                    
+                    <!-- Search
+                    -------------------------------------------------------------------->
+                    
+                    <div id="search"><?php echo tabIndent($searchForm, 3); ?></div>
+        
+                    <!-- User Info
+                    -------------------------------------------------------------------->
+                    
+                    <?php if(!$user->isGuest()): ?>
+            
+                    <ul id="controls">
+        
+                        <?php if($user->hasPermission('profile-edit')): ?>
+                        <li><a class="tip profile" href="<?php echo $config->urls->admin; ?>profile/" title="<?php echo ucfirst (__('profile', __FILE__)); ?>"><?php echo ucfirst (__('profile', __FILE__)); ?></a></li>
+                        <?php endif; ?>
+            
+                        <li><a class="tip view" href="<?php echo $config->urls->root; ?>" title="<?php echo ucfirst (__('Site', __FILE__)); ?>"><?php echo ucfirst (__('Site', __FILE__)); ?></a></li>
+            
+                        <li><a class="tip logout" href="<?php echo $config->urls->admin; ?>login/logout/" title="<?php echo ucfirst (__('logout', __FILE__)); ?>"><?php echo ucfirst (__('logout', __FILE__)); ?></a></li>
+              
+                    </ul>
+                
+                    <?php endif; ?>
+
+                    <!-- Breadcrumbs
+                    -------------------------------------------------------------------->
+        
+                    <?php if(!$user->isGuest()) {
+                    
+                    echo "<ul id='breadcrumbs'>";
+                    foreach($this->fuel('breadcrumbs') as $breadcrumb) {
+                            $title = __($breadcrumb->title, __FILE__); 
+                            echo "\n\t\t\t\t<li><a href='{$breadcrumb->url}'>{$title}</a></li>";
+                        }
+                    echo "</ul>";
+    
+                    } ?>
+                    
+                    <!-- Title
+                    -------------------------------------------------------------------->
+                
+                </div><!-- Header END-->
+                
+                <h1 class="title"><?php echo __(strip_tags($this->fuel->processHeadline ? $this->fuel->processHeadline : $page->get("title|name")), __FILE__); ?></h1>
+    
+                <!-- Notices
+                -------------------------------------------------------------------->
+    
+                <?php if(count($notices)) include($config->paths->adminTemplates . "notices.inc"); ?>
+        
+                <?php if(trim($page->summary)) echo "<h2>{$page->summary}</h2>"; ?>
+                
+                <?php if($page->body) echo $page->body; ?>
+    
+                <?php echo $content ?>
+    
+                <?php if($config->debug && $this->user->isSuperuser()) include($config->paths->adminTemplates . "debug.inc"); ?>
+    
+                <!-- Footer
+                -------------------------------------------------------------------->
+    
+                <div id="footer" class="footer">
+                    
+                    <div class="container">
+                        
+                        <div id="left">
+                            
+                            <a href="http://www.processwire.com">ProcessWire <?php echo $config->version . '.' . $config->systemVersion; ?></a> <span>/</span> Copyright &copy; <?php echo date("Y"); ?> by Ryan Cramer
+                        
+                        </div>
+                        
+                        <div id="right">
+                        
+                            <div id="code">
+                            
+                                <?php echo "This page was created in <span>". Debug::timer('ergo') ."</span> seconds"; ?>
+                            
+                            </div>
+                        
+                            <div id="top"><a href="#">Top</a></div>
+                        
+                        </div>
+                
+                    </div>
+                
+                </div><!-- Footer END-->
+                
+			</div><!-- Right END-->
+            
+            <div class="clear"></div>
+    
+		</div><!-- Container END-->
+
+	</div><!-- Content END-->
+
+</div><!-- Wrapper END-->
+
+</body>
+
+<?php } ?>
+
+</html>
---
+++ scripts/main.js
@@ -0,0 +1,188 @@
+$(document).ready(function() {
+
+	var $sidebar = $("#sidebar"), $window = $(window), offset = $sidebar.offset(), topPadding = 150;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 50
+            });
+        }
+    });
+	
+	var fullWidth = {'width' : '1280px'}
+	var minWidth = {'width' : '1080px'}
+
+	// Collapsible Sidebar //
+    if ($.cookie("sidebar") == "collapsed") {
+        $('#toggle').html("Expand Sidebar");
+		$('#toggle').css('left', '-20px');
+        $("#content").css(fullWidth);
+        $("#sidebar").hide();
+    }
+
+    $('#toggle').click(function(event) {
+        event.preventDefault();
+
+        if ($('#sidebar').is(':visible')) {
+
+            $('#toggle').html("Expand Sidebar");
+
+            $("#sidebar").stop(true, true).fadeOut("slow", function() {
+                $("#content").stop(true, true).animate(fullWidth, 500);
+				$('#toggle').css('left', '-20px');
+                $.cookie("sidebar", "collapsed", { expires: 365, path: '/' });
+            });
+        }
+        else {
+			
+            $('#toggle').html("Collapse Sidebar");
+
+            $("#content").stop(true, true).animate(minWidth, 500, function() {
+				$("#sidebar").stop(true, true).fadeIn("slow");
+				$('#toggle').css('left', '180px');
+                $.cookie("sidebar", "expanded", { expires: 365, path: '/' });
+            });
+        }
+    });
+
+	function accordion() {
+		
+		$('#nav ul').hide();
+		
+		$.each($('#nav'), function(){
+			
+			var cookie = $.cookie(this.id);
+			
+			if(cookie === null || String(cookie).length < 1) {
+				$('#' + this.id + '.expandfirst ul:first').show();
+			} else {
+				$('#' + this.id + ' .' + cookie).next().show();
+			}
+		});
+	
+	$('#nav li a').click(function() {
+	
+		var checkElement = $(this).next();
+		var parent = this.parentNode.parentNode.id;
+		
+		if($('#' + parent).hasClass('accordion')) {
+			if((String(parent).length > 0) && (String(this.className).length > 0)) {
+				if($(this).next().is(':visible')) {
+					$.cookie(parent, null);
+				} else {
+					$.cookie(parent, this.className);
+				}
+				$(this).next().slideToggle('fast');
+			}
+		}
+		
+		if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+			if($('#' + parent).hasClass('collapsible')) {
+				$('#' + parent + ' ul:visible').slideUp('fast');
+			}
+			return false;
+		}
+	
+		if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+			$('#' + parent + ' ul:visible').slideUp('fast');
+			if((String(parent).length > 0) && (String(this.className).length > 0)) {
+				$.cookie(parent, this.className);
+			}
+			checkElement.slideDown('fast');
+			return false;
+			}
+		});
+	}
+
+	accordion();
+
+	// setup the toggles for Inputfields and the animations that occur between opening and closing
+	$(".Inputfields > li > label.ui-widget-header").addClass("InputfieldStateToggle")
+		.prepend("<span class='ui-icon ui-icon-carat-1-s'></span>")
+		.click(function() {
+			var $li = $(this).parent('li'); 	
+			$li.toggleClass('InputfieldStateCollapsed', 100);
+			$(this).children('span.ui-icon').toggleClass('ui-icon-carat-1-e ui-icon-carat-1-s'); 
+			$li.children('.ui-widget-header').effect('highlight', {color:"#fff"}, 300); 
+			return false;
+		})
+
+	// use different icon for open and closed
+	$(".Inputfields .InputfieldStateCollapsed > label.ui-widget-header span.ui-icon")
+		.removeClass('ui-icon-carat-1-s').addClass('ui-icon-carat-1-e'); 
+
+
+	// if there are buttons in the format "a button" without ID attributes, copy them into the masthead
+	// or buttons in the format button.head_button_clone with an ID attribute.
+	var $buttons = $("#content a[id=] button[id=], #content button.head_button_clone[id!=]"); 
+	if($buttons.size() > 0) {
+		var $head = $("<div id='head_button'></div>").appendTo("#content .right").show();
+		$buttons.each(function() {
+			var $t = $(this);
+			var $a = $t.parent('a'); 
+			if($a.size()) { 
+				$button = $t.parent('a').clone();
+				$head.append($button);
+			} else if($t.is('.head_button_clone')) {
+				$button = $t.clone();
+				$button.attr('data-from_id', $t.attr('id')).attr('id', $t.attr('id') + '_copy');
+				$a = $("<a></a>").attr('href', '#');
+				$button.click(function() {
+					$("#" + $(this).attr('data-from_id')).click().parents('form').submit();
+					return false;
+				});
+				$head.append($a.append($button));	
+			}
+		}); 
+	}
+
+	// make buttons with <a> tags click to the href of the <a>
+	$("a > button").click(function() {
+		window.location = $(this).parent("a").attr('href'); 
+	}); 
+
+	// we don't even want to go there
+	if($.browser.msie && $.browser.version < 8) {
+		$("#content .container").html("<h2>ProcessWire does not support IE7 and below at this time. Please try again with a newer browser.</h2>").show();
+	}
+
+	// add focus to the first text input, where applicable
+	jQuery('#content input[type=text]:visible:enabled:first').each(function() {
+		var $t = $(this); 
+		if(!$t.val() && !$t.is(".no_focus")) $t.focus();	
+	});
+
+	$('#top a').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+			}, 200);
+		return false;
+	});
+	
+	$(".tip").tipTip({delay:200,fadeIn:100,fadeOut:100});
+	
+	if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0)
+	{
+		var _interval = window.setInterval(function ()
+		{
+			var autofills = $('input:-webkit-autofill');
+			if (autofills.length > 0)
+			{
+				window.clearInterval(_interval); // stop polling
+				autofills.each(function()
+				{
+					var clone = $(this).clone(true, true);
+					$(this).after(clone).remove();
+				});
+			}
+		}, 200);
+	}
+	
+	jQuery('#content').removeClass('fouc_fix');
+
+});
